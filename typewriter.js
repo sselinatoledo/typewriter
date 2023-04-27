b37b9408afb8434048d963e727f466e69d5ad433
sselinatoledo <sselinatoledo@gmail.com>
2023-04-27 22:52:26 +0000

    eslint typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,11 @@
 const sentence = "hello there from lighthouse labs";
-let time = 0
+let time = 0;
 for (const char of sentence) {
-    setTimeout(() => {
-        process.stdout.write(char);
-    }, time)
-    time += 50;
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, time);
+  time += 50;
 }
 setTimeout(() => {
-    console.log("\n");
-}, time)
+  console.log("\n");
+}, time);
